a1bc5c65b3c902406f47b404042a945df1fbdd3e
Justin Merz <jrmerz@gmail.com>
2023-11-11 15:51:07 -0800

    switching to ckan provided solr image

--- cmds/build.sh
+++ cmds/build.sh
@@ -15,32 +15,20 @@ if [[ -z $CLOUD_BUILD ]]; then
   export DOCKER_BUILDKIT=1
 fi
 
-SOLR_VERSION=$SOLR_TAG
-
 # Additionally set local-dev tags used by 
 # local development docker compose file
 if [[ ! -z $LOCAL_BUILD ]]; then
   echo "local build"
   SEARCH_TAG='local-dev'
   DATA_TAG='local-dev'
-  SOLR_TAG='local-dev'
 fi
 
 SEARCH_REPO_HASH=$(git -C $REPOSITORY_DIR/$SEARCH_REPO_NAME log -1 --pretty=%h)
 DATA_REPO_HASH=$(git -C $REPOSITORY_DIR/$DATA_REPO_NAME log -1 --pretty=%h)
 
-echo "building solr $SOLR_IMAGE_NAME:$SOLR_TAG"
 echo "building ckan $CKAN_IMAGE_NAME:$CKAN_TAG"
 echo "building ecosis data $DATA_IMAGE_NAME:$DATA_TAG"
 echo "building ecosis search $SEARCH_IMAGE_NAME:$SEARCH_TAG"
-
-# solr
-echo "building solr $SOLR_IMAGE_NAME:$SOLR_TAG"
-docker build \
-  -t $SOLR_IMAGE_NAME:$SOLR_TAG \
-  --build-arg SOLR_VERSION=${SOLR_VERSION} \
-  --cache-from=$SOLR_IMAGE_NAME:$DOCKER_CACHE_TAG \
-  ./containers/solr
 
 # ckan
 echo "building ckan $CKAN_IMAGE_NAME:$CKAN_TAG"
--- containers/solr/init-mount.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# this script goes in the /docker-entrypoint-initdb.d dir
-# It must be run AFTER the container is started, when the
-# data directory is mounted.  In docker this mount point
-# is mounted as root, causing solr to fail
-sudo chown -R solr:solr /opt/solr/server/solr/ckan/data
