bb8fd0db7c32886c00d6def985a06b14dc02f247
Justin Merz <jrmerz@gmail.com>
2025-03-19 07:37:15 -0700

    increasing file cleanup times

--- containers/tmp-cleanup/tmp-cleanup.sh
+++ containers/tmp-cleanup/tmp-cleanup.sh
@@ -3,4 +3,5 @@
 DIRECTORY="/tmp/Ecosis/sessions"
 
 # Find and delete files older than 1 day
-find "$DIRECTORY" -type f -mtime +1 -exec rm {} +
+# find "$DIRECTORY" -type f -mtime +1 -exec rm {} +
+find "$DIRECTORY" -type f -mmin +720 -exec rm {} +
